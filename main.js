e5ffd6cdb327584807f5be4b667d294beb4a474d
Hilal Koujil <hilal1993@hotmail.be>
2021-11-22 11:36:23 +0100

    exo8

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1 
+let un = document.getElementsByClassName("redPurple");
+console.log(un);
+// 2
+let deux = document.querySelectorAll(".redPurple");
+console.log(deux);
+
+// 3
+
+let trois = document.querySelector("h1.redPurple");
+console.log(trois);
+
+// 4
+let quatre = document.querySelectorAll("li,p,span");
+console.log(quatre);
+// 5
+let cinq = document.querySelectorAll("li.important,p");
+console.log(cinq);
+// 6
+let six = document.querySelectorAll("h1.redPurple,span.redPurple");
+console.log(six);
